c61d2415576b57b1d0963866c90609c1de8efa92
kameronhnath <kameron.hnath@gmail.com>
2018-01-25 17:28:08 -0500

    added vector calculations and encoder functions to autoVuMark

--- RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/temp8121/testVuAuto.java
+++ RMRobotics/src/main/java/org/firstinspires/ftc/rmrobotics/opmodes/temp8121/testVuAuto.java
@@ -3,6 +3,10 @@ package org.firstinspires.ftc.rmrobotics.opmodes.temp8121;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -23,13 +27,60 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  * Created by rotom on 10/17/2017.
  */
 
-@Autonomous(name="testVuAuto", group ="empty")
+@Autonomous(name="testVuAuto", group ="woRMholeConfig")
 public class testVuAuto extends LinearOpMode{
+
+    private ColorSensor colorSensor;
+    private DcMotor wheelFL;
+    private DcMotor wheelFR;
+    private DcMotor wheelBL;
+    private DcMotor wheelBR;
+    private DcMotor lift;
+    //    private DcMotor arm;
+    private Servo clawBL;
+    private Servo clawBR;
+    private Servo clawTL;
+    private Servo clawTR;
+    //    private Servo armT;
+    private Servo gemBar;
+    //    private CRServo armB;
+
+    static final double  tickRev = 1120 ;    // eg: TETRIX Motor Encoder
+    static final double  invGearRatio = 0.5 ;     // This is < 1.0 if geared UP
+    static final double  diameterBoi = 101.6 ;     // For figuring circumference
+    static final double  tickMM  = (tickRev * invGearRatio) /
+            (diameterBoi * 3.1415);
+
+    private ElapsedTime runtime = new ElapsedTime();
+
     public static final String TAG = "Vuforia VuMark Sample";
     OpenGLMatrix lastLocation = null;
     VuforiaLocalizer vuforia;
+
     @Override public void runOpMode() {
 
+        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+        wheelFL = hardwareMap.dcMotor.get("wheelFL");
+        wheelFR = hardwareMap.dcMotor.get("wheelFR");
+        wheelBL = hardwareMap.dcMotor.get("wheelBL");
+        wheelBR = hardwareMap.dcMotor.get("wheelBR");
+        lift = hardwareMap.dcMotor.get("lift");
+        clawBL = hardwareMap.servo.get("clawBL");
+        clawBR = hardwareMap.servo.get("clawBR");
+        clawTL = hardwareMap.servo.get("clawTL");
+        clawTR = hardwareMap.servo.get("clawTR");
+        gemBar = hardwareMap.servo.get("gemBar");
+
+//    static final double     DRIVE_SPEED             = 0.6;
+//    static final double     TURN_SPEED              = 0.5;
+
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        wheelBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheelBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheelFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheelFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
@@ -46,6 +97,7 @@ public class testVuAuto extends LinearOpMode{
         float mmPerInch        = 25.4f;
         float mmBotWidth       = 18 * mmPerInch;            // ... or whatever is right for your robot
         float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch;   // the FTC field is ~11'10" center-to-center of the glass panels
+
 
 
         //0,0,0 is front left bottom corner. phone is a matrix of where the phone is relative to the robot
@@ -65,23 +117,42 @@ public class testVuAuto extends LinearOpMode{
                 .multiplied(Orientation.getRotationMatrix(
                         AxesReference.EXTRINSIC, AxesOrder.XZY,
                         AngleUnit.DEGREES, 0, 0, 90));
-        //kameron is actually tilted
-        while (opModeIsActive()) {
-            OpenGLMatrix bot2field = new OpenGLMatrix();
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                telemetry.addData("VuMark", "%s visible", vuMark);
-                OpenGLMatrix camera2pic = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
-                //pic2phone.translation(0.0f, 0.0f, -90.0f);
 
-                if (camera2pic != null) {
-                    camera2pic.rotated(AngleUnit.DEGREES, -90, 0, 1, 0);
-                    OpenGLMatrix bot2pic = camera2pic.multiplied(camera2bot);
-                    bot2field = bot2pic.multiplied(pic2field);
-                    telemetry.addData("c2p", format(camera2pic));
-                    telemetry.addData("b2p", format(bot2pic));
-                    telemetry.addData("b2f", format(bot2field));
-                    VectorF trans = camera2pic.getTranslation();
+        waitForStart();
+        while (opModeIsActive()) {
+
+            OpenGLMatrix camera2pic = null;
+
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.UNKNOWN;
+
+            while(vuMark == RelicRecoveryVuMark.UNKNOWN) {
+                vuMark = RelicRecoveryVuMark.from(relicTemplate);
+                if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+                    telemetry.addData("VuMark", "%s visible", vuMark);
+                    camera2pic = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
+                } else {
+                    telemetry.addData("VuMark", "not visible");
+                }
+            }
+            //pic2phone.translation(0.0f, 0.0f, -90.0f);
+            camera2pic.rotated(AngleUnit.DEGREES, -90, 0, 1, 0);
+            OpenGLMatrix bot2pic = camera2pic.multiplied(camera2bot);
+            OpenGLMatrix bot2field = new OpenGLMatrix();
+            bot2field = bot2pic.multiplied(pic2field);
+            telemetry.addData("c2p", format(camera2pic));
+            telemetry.addData("b2p", format(bot2pic));
+            telemetry.addData("b2f", format(bot2field));
+            if (vuMark == RelicRecoveryVuMark.LEFT) {
+                VectorF move = getVector(bot2field, 609.6f, 1939.798f);
+            }
+            if (vuMark == RelicRecoveryVuMark.CENTER) {
+                VectorF move = getVector(bot2field, 609.6f, 2133.6f);
+            }
+            if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                VectorF move = getVector(bot2field, 609.6f, 2327.402f);
+            }
+
+
 //                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
 //                    double tX = trans.get(0);
 //                    double tY = trans.get(1);
@@ -93,146 +164,102 @@ public class testVuAuto extends LinearOpMode{
 
                     //offset picture from phone
                     //we need robot to picture
-                }
-            }
 
-            else {
-                telemetry.addData("VuMark", "not visible");
-            }
             telemetry.update();
 
+            wheelFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            // Send telemetry message to indicate successful Encoder reset
+            telemetry.addData("Path0",  "Starting at %7d :%7d",
+                    wheelFL.getCurrentPosition());
+            telemetry.update();
+
+
+            // Step through each leg of the path,
+            // Note: Reverse movement is obtained by setting a negative distance (not speed)
+            encoderDrive(0.5,48,5.0);  // S1: Forward 47 Inches with 5 Sec timeout
         }
     }
     String format(OpenGLMatrix transformationMatrix) {
         return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
     }
+
+    /*
+     *  Method to perfmorm a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double dist,
+                             double timeoutS) {
+        int target;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            target = wheelFL.getCurrentPosition() + (int)(dist * tickMM);
+            wheelFL.setTargetPosition(target);
+            wheelBL.setTargetPosition(target);
+            wheelFR.setTargetPosition(target);
+            wheelBR.setTargetPosition(target);
+
+            // Turn On RUN_TO_POSITION
+            wheelFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            wheelBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+
+            wheelFL.setPower(Math.abs(speed));
+            wheelFR.setPower(Math.abs(speed));
+            wheelBL.setPower(Math.abs(speed));
+            wheelBR.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (wheelFL.isBusy() && wheelFR.isBusy() && wheelBL.isBusy() && wheelBR.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to ", target);
+                telemetry.addData("Path2",  "Running at ", wheelFL.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            wheelFL.setPower(0);
+            wheelFL.setPower(0);
+            wheelFL.setPower(0);
+            wheelFL.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            wheelFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            wheelBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
+    }
+
+    public VectorF getVector(OpenGLMatrix bot, float desX, float desY)
+    {
+        VectorF amHere = bot.getTranslation();
+        VectorF goThere = new VectorF(0, desX ,desY);
+        return goThere.subtracted(amHere);
+    }
 }
-//=======
-//package org.firstinspires.ftc.rmrobotics.opmodes.temp8121;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.util.RobotLog;
-//
-//import org.firstinspires.ftc.robotcore.external.ClassFactory;
-//import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-//import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-//import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-//import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-//import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-//import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-//import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
-//import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-//import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-//import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
-//import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
-//
-///**
-// * Created by rotom on 10/17/2017.
-// */
-//
-//@Autonomous(name="testVuAuto", group ="empty")
-//public class testVuAuto extends LinearOpMode {
-//    public static final String TAG = "Vuforia VuMark Sample";
-//    OpenGLMatrix lastLocation = null;
-//    VuforiaLocalizer vuforia;
-//
-//    @Override
-//    public void runOpMode() {
-//
-//
-//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-//        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-//        parameters.vuforiaLicenseKey = "AckoWtn/////AAAAGan7WAnq/0UVmQZG3sp7smBgRCNBnU1p+HmsTrC+W9TyxqaMlhFirDXglelvJCX4yBiO8oou6n7UWBfdRFbKHDqz0NIo5VcNHyhelmm0yK0vGKxoU0NZbQzjh5qVWnI/HRoFjM3JOq/LB/FTXgCcEaNGhXAqnz7nalixMeP8oRQlgX5nRVX4uE6w0K4yqIc5/FIDh1tn7PldiflmvNPhOW6FukPQD3d02wEnZB/JEchSSBzDbFA10XSgtYzXiweQI5tj+D5llLRrLh0mcWeouv55oSmya5RxUC26uEuO7bCAwyolWIuUr2Wh5oAG483nTD4vFhdjVMT7f0ovLO73C6xr2AXpNwen9IExRxBeosQ4";
-//        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
-//        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-//        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
-//        VuforiaTrackable relicTemplate = relicTrackables.get(0);
-//        relicTemplate.setName("relicVuMarkTemplate");
-////        telemetry.addData(">", "Press Play to start");
-//        telemetry.update();
-//        waitForStart();
-//        relicTrackables.activate();
-//
-//        //0,0,0 is front left bottom corner. phone is a matrix of where the phone is relative to the robot
-//        OpenGLMatrix camera2bot = OpenGLMatrix
-//                .translation(3.75f, 5.37f, 6.248f) //temporary and arbitrary values. goal is roughly closer to the front, far right side, near the bottom ish
-//                .multiplied(Orientation.getRotationMatrix(
-//                        AxesReference.EXTRINSIC, AxesOrder.XZY,
-//                        AngleUnit.DEGREES, 0, 0, 0));
-//        //theoretically should have phone upright, screen facing out towards the right
-////        RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
-//
-//        //trans: bottom left of field is 0,0. red bottom picture is (0,34,0) in inches. blue bottom (144,34,0) red top (0,106,0) blue top (144,106,0)
-//        //rot: (0,0,90) (0,0,-90), (0,0,0), (0,0,0)
-//        //this program is testing red top
-//        OpenGLMatrix pic2field = OpenGLMatrix
-//                .translation(0, 106, 0) //temporary and arbitrary values. goal is roughly closer to the front, far right side, near the bottom ish
-//                .multiplied(Orientation.getRotationMatrix(
-//                        AxesReference.EXTRINSIC, AxesOrder.XZY,
-//                        AngleUnit.DEGREES, 0, 0, 90));
-////<<<<<<< HEAD
-//
-//        OpenGLMatrix pic2phone = null;
-//
-////=======
-//        //kameron is actually tilted
-////>>>>>>> d118fb6a8d449c1eb830241cfa0ccbda41677750
-//        while (opModeIsActive()) {
-//            OpenGLMatrix bot2field = new OpenGLMatrix();
-//            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-//            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-////<<<<<<< HEAD
-//                //telemetry.addData("VuMark", "%s visible", vuMark);
-//
-//                pic2phone = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
-//                //telemetry.addData("Pose", format(pic2phone));
-//                if (pic2phone != null) {
-//                    OpenGLMatrix pic2bot = pic2phone.multiplied(phone2bot);
-//                    OpenGLMatrix bot2pic = pic2bot.transposed();
-////=======
-//                    telemetry.addData("VuMark", "%s visible", vuMark);
-//                    OpenGLMatrix camera2pic = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
-//                    //pic2phone.translation(0.0f, 0.0f, -90.0f);
-//
-//                    if (camera2pic != null) {
-//                        bot2pic = camera2pic.multiplied(camera2bot);
-////>>>>>>> d118fb6a8d449c1eb830241cfa0ccbda41677750
-//                        bot2field = bot2pic.multiplied(pic2field);
-//                        telemetry.addData("c2p", format(camera2pic));
-//                        telemetry.addData("b2p", format(bot2pic));
-//                        telemetry.addData("b2f", format(bot2field));
-//                        VectorF trans = camera2pic.getTranslation();
-////                    Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-////                    double tX = trans.get(0);
-////                    double tY = trans.get(1);
-////                    double tZ = trans.get(2);
-////                    double rX = rot.firstAngle;
-////                    double rY = rot.secondAngle;
-////                    double rZ = rot.thirdAngle;
-//
-//
-//                        //offset picture from phone
-//                        //we need robot to picture
-//                    }
-//                } else {
-//                    telemetry.addData("VuMark", "not visible");
-//                }
-////<<<<<<< HEAD
-//                telemetry.update();
-//                telemetry.addData("p2f", format(pic2field));
-//                telemetry.addData("p2ph", format(pic2phone));
-////=======
-//                telemetry.update();
-////>>>>>>> d118fb6a8d449c1eb830241cfa0ccbda41677750
-//
-//            }
-//        }
-//    }
-//        String format (OpenGLMatrix transformationMatrix){
-//            return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
-//        }
-//    }
-//}
-//>>>>>>> ad0070b3b4754098fabb38296687ea6389203edf
